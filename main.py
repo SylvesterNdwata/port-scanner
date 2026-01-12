80177750fb9eecae8d7497648248df722c6dcb8e
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-01-12 12:56:29 +0100

    argument parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import argparse
 
 def scanner(ip, start_port, end_port):
     print(f"Scanning {ip} from {start_port} to {end_port}")
@@ -12,9 +13,18 @@ def scanner(ip, start_port, end_port):
             print(f"[+] port {port} is open")
         sock.close()
     
+def create_parser():
+    parser = argparse.ArgumentParser(prog="PortScanner", description="A simple port scanner tool")
+    parser.add_argument('ip', type=str, help='Target IP address to scan')
+    parser.add_argument("-s", "--start_port", type=int, default=1, help="Starting port number (default: 1)")
+    parser.add_argument("-e", "--end_port", type=int, default=1024, help="Ending port number (default: 1024)")
+    args = parser.parse_args()
+
+    scanner(ip=args.ip, start_port=args.start_port, end_port=args.end_port)
 
 if __name__ == "__main__":
     target = "127.0.0.1"
     scanner(target, 1, 1024)
-
+else:
+    create_parser()
 
